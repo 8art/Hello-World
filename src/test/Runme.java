b04edc2ca5c08b6ca504db4d3abea33454ee4faf
8art <omnipotent@o2.pl>
2011-06-28 22:20:46 +0300

    Added HelloWorld class and changed settings

---
+++ src/test/Runme.java
@@ -0,0 +1,19 @@
+/**
+ * 8art - Hello-World - 22:18:08.
+ */
+package test;
+
+/**
+ * @author 8art
+ */
+public class Runme {
+
+  /**
+   * @author 8art
+   * @param args
+   */
+  public static void main(final String[] args) {
+    System.out.println("Hello World");
+  }
+
+}
